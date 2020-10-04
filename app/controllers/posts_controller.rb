077c2f40da1183472e0693ee1439daa8e4060eed
andou-0307 <703515412307a@gmail.com>
2020-10-04 11:18:47 +0900

    create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,15 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    Post.create(post_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:text)
+  end
 end
